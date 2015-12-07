13aa6de5ad5037a359431544f10277bcd4043fac
Alex Rao <YOUR_GITHUB_EMAIL_ADDRESS>
2015-12-06 22:19:36 -0800

    Got some stuff working and some stuff I can't get to work

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,69 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-// your code
+	var baseUrl = '/api/movies';
+
+	var allMovies = [];
+
+	var $moviesList = $('#movies-list');
+
+	var $createMovie = $('#create-movie');
+
+	var source = $('#movies-template').html();
+	var template = Handlebars.compile(source);
+
+	var render = function() {
+		$moviesList.empty();
+		var moviesHtml = template({ movies: allMovies });
+		$moviesList.append(moviesHtml);
+	};
+
+	$.ajax({
+		method: 'GET',
+		url: baseUrl,
+		success: function(taco) {
+			allMovies = taco.movies
+			render();
+		}
+	});
+
+	$createMovie.on('submit', function (event) {
+    	event.preventDefault();
+
+    	var newMovie = $(this).serialize();
+
+    	$.post(baseUrl, newMovie, function (data) {
+      		console.log(data);
+    		allMovies.push(data);
+    		render();
+    	});
+
+    	$createMovie[0].reset();
+    	$createMovie.find('input').first().focus();
+	});
+
+	$moviesList
+
+    .on('submit', '.update-movie', function (event) {
+    	event.preventDefault();
+      
+    	var movieId = $(this).closest('.movie').attr('data-id');
+
+    	var movieToUpdate = allMovies.filter(function (movie) {
+    	return movie._id == movieId;
+    	})[0];
+
+    	var updatedMovie = $(this).serialize();
+
+    	$.ajax({
+      		type: 'PUT',
+      		url: baseUrl + '/' + movieId,
+      		data: updatedMovie,
+      		success: function(data) {
+        		allMovies.splice(allMovies.indexOf(MovieToUpdate), 1, data);
+        		render();
+      	}
+    	});
+    });
 
 });
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 // require express and other modules
 var express = require('express'),
     app = express();
+    bodyParser = require('body-parser');
 
 // serve static files from public folder
 app.use(express.static(__dirname + '/public'));
+
+// configure bodyParser (for receiving form data)
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Allow CORS: we'll use this today to reduce security so we can more easily test our code in the browser.
 app.use(function(req, res, next) {
@@ -16,7 +20,20 @@ app.use(function(req, res, next) {
  * DATABASE *
  ************/
 
-// your hardcoded data here
+var movies = [
+  { _id: 1, title: 'Beast of No Nation', director: 'Cary Fukunaga' },
+  { _id: 2, title: 'Howl\'s moving castle', director: 'Hayao Miyazaki' },
+  { _id: 3, title: 'Snatch', director: 'Guy Ritchie' }
+];
+
+var profile = [
+  {name: "Alex Rao", github_link: "<a href=\"https://github.com/alexpsu\">", curent_city: "Palo Alto", family_members: [
+  {name: "Pradeep Rao", relationship: "father"},
+  {name: "Amy Rao", relationship: "mother"},
+  {name: "Ahna Rao", relationship: "sister"}
+  ]
+  }
+];
 
 /**********
  * ROUTES *
@@ -41,9 +58,92 @@ app.get('/api', function api_index (req, res){
     documentation_url: "https://github.com/alexpsu/express_self_api/README.md", // CHANGE THIS TO LINK TO YOUR README.md
     base_url: "http://hidden-reaches-6287.herokuapp.com",
     endpoints: [
-      {method: "GET", path: "/api", description: "Describes available endpoints"}
+      {method: "GET", path: "/api/profile", description: "A little about me!"},
+      {method: "GET", path: "/api/movies", description: "A list of my favorite movies"},
+      {method: "GET", path: "/api/movies/:id", description: "A specific movie"},
+      {method: "GET", path: "/api/movies/search", description: "Search's for a specific movie"},
+      {method: "POST", path: "/api/movies", description: "Post a movie"},
+      {method: "PUT", path: "/api/movies/:id", description: "Edit a movie"},
+      {method: "DELETE", path: "/api/movies/:id", description: "Delete a movie"}
     ]
   })
+});
+
+app.get('/api/profile', function index(req, res) {
+  res.json({profile});
+});
+
+app.get('/api/movies', function index(req, res) {
+  res.json({movies});
+});
+
+app.get('/api/movies/:id', function show(req, res) {
+  var idReq = req.params.id;
+  var found;
+  movies.forEach(function(ele) {
+    if(ele._id == idReq) {
+      found = movies.indexOf(ele);
+      console.log(found);
+    } else {
+      console.log("dud")
+    }
+  })
+  res.json(movies[found]);
+});
+
+app.get('/api/movies/search', function search(req, res){
+  var newSearch = req.query.q.toString();
+  var filterMovie;
+  var found;
+  console.log(movies);
+  movies.forEach(function(ele, index) {
+    console.log(ele);
+    if(ele.title == newSearch) {
+      filterMovie = movies.splice(index, 1);
+      console.log(filterMovie);
+    } else {
+      console.log("dud");
+      console.log(filterMovie);
+    }
+  })
+  res.jston({movies});
+});
+
+app.post('/api/movies', function create(req, res) {
+  var newMovie;
+  var newid;
+  if( movies.length > 0) {
+    newid = movies[movies.length - 1]._id + 1
+  } else {
+    newid = 1;
+  }
+  var newTitle = req.body.title;
+  var newDirector = req.body.director;
+  newMovie = { _id: newid, title: newTitle, director: newDirector}
+  movies.push(newMovie);
+  res.json(newMovie);
+});
+
+app.put('/api/movies/:id', function update(req, res) {
+  var idReq = parseInt(req.params.id);
+  var newTitle = req.body.title;
+  var newDirector = req.body.director;
+  var updateMovie = { _id: idReq, title: newTitle, director: newDirector};
+  res.json(updateMovie);
+});
+
+app.delete('/api/movies/:id', function destroy(req, res) {
+  var idReq = parseInt(req.params.id);
+  var found;
+  movies.forEach(function(ele, index) {
+    if(ele._id === idReq) {
+      found = index;
+    } else {
+      console.log("dud");
+    }
+  })
+  var newMovies = movies.splice(found, 1);
+  res.json(newMovies);
 });
 
 /**********

ee867dd78f9a5d5bbc0a6bc8ac5965151a24e6f2
Alex Rao <YOUR_GITHUB_EMAIL_ADDRESS>
2015-12-07 18:04:37 -0800

    I commited to heroku. This is hell

--- public/js/app.js
+++ public/js/app.js
@@ -2,71 +2,102 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-	var baseUrl = '/api/movies';
+  var source = $('#movies-template').html();
 
-	var allMovies = [];
+  var template = Handlebars.compile(source);
 
-	var $moviesList = $('#movies-list');
+  var baseUrl = '/api/movies';
 
-	var $createMovie = $('#create-movie');
+  var allMovies = [];
 
-	var source = $('#movies-template').html();
-	var template = Handlebars.compile(source);
+  var $moviesList = $('#movies-list');
 
-	var render = function() {
-		$moviesList.empty();
-		var moviesHtml = template({ movies: allMovies });
-		$('#moviesList').append(moviesHtml);
-	};
+  var $createMovie = $('#create-movie');
 
-	$.ajax({
-		method: 'GET',
-		url: baseUrl,
-		success: function(taco) {
-			allMovies = taco.movies
-      allMovies.forEach(function(ele){
-      $('#movies-list').append("<p>" + ele.title + " by " + ele.director + "</p>");  
-      })
-		}
-	});
+  var newTitle;
+  var newDirector;
+  var newId;
 
-	$createMovie.on('submit', function (event) {
-    	event.preventDefault();
 
-    	var newMovie = $(this).serialize();
+  var moviesHtml;
 
-    	$.post(baseUrl, newMovie, function (data) {
-      	console.log(data);
-    		allMovies.push(data);
-    		render();
-    	});
+  $.ajax({
+    method: 'GET',
+    url: baseUrl,
+    success: function(taco) {
+      allMovies = taco.movies;
+      console.log(allMovies);
+      moviesHtml = template({movies: allMovies});
+      $('#movies-list').append(moviesHtml);
+    }
+  });
 
-    	$createMovie[0].reset();
-    	$createMovie.find('input').first().focus();
-	});
+  $createMovie.on('submit', function (event) {
+    event.preventDefault();
+    var newMovie = $('#create-movie').serialize();
+    newMovie._id = (allMovies.length + 1);
+    console.log(newMovie);
+    $.ajax({
+      method: 'POST',
+      url: baseUrl,
+      data: newMovie,
+      success: function(data) {
+        $('#movies-list').append("<p>" + data.title + " by " + data.director + "</p>");
+      }
+    })
+  });
 
-	$moviesList
+  $moviesList
 
     .on('submit', '.update-movie', function (event) {
-    	event.preventDefault();
+      event.preventDefault();
       
-    	var movieId = $(this).closest('.movie').attr('data-id');
+      var movieId = $(this).closest('.movie').attr('data-id');
 
-    	var movieToUpdate = allMovies.filter(function (movie) {
-    	return movie._id == movieId;
-    	})[0];
+      var movieToUpdate = allMovies.filter(function (movie) {
+      return movie._id == movieId;
+      })[0];
 
-    	var updatedMovie = $(this).serialize();
+      var updatedMovie = $(this).serialize();
 
-    	$.ajax({
-      		type: 'PUT',
-      		url: baseUrl + '/' + movieId,
-      		data: updatedMovie,
-      		success: function(data) {
-        		allMovies.splice(allMovies.indexOf(MovieToUpdate), 1, data);
-        		render();
-      	}
-    	});
+      $.ajax({
+          type: 'PUT',
+          url: baseUrl + '/' + movieId,
+          data: updatedMovie,
+          success: function(data) {
+            allMovies.splice(allMovies.indexOf(movieToUpdate), 1, data);
+            $moviesList.empty();
+            moviesHtml = template({movies: allMovies});
+            $('#movies-list').append(moviesHtml);
+          }
+      });
+    })
+
+    .on('click', '.delete-movie', function (event) {
+      event.preventDefault();
+
+      // find the todo's id (stored in HTML as `data-id`)
+      var movieId = $(this).closest('.movie').attr('data-id');
+
+      // find the todo to delete by its id
+      var movieToDelete = allMovies.filter(function (movie) {
+        return movie._id == movieId;
+      })[0];
+
+      // DELETE request to delete todo
+      $.ajax({
+        type: 'DELETE',
+        url: baseUrl + '/' + movieId,
+        success: function(data) {
+          // remove deleted todo from all todos
+          allMovies.splice(allMovies.indexOf(movieToDelete), 1);
+
+          // render all todos to view
+          $moviesList.empty();
+          moviesHtml = template({movies: allMovies});
+          $('#movies-list').append(moviesHtml);
+        }
+      });
     });
 
 });
